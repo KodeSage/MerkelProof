f9eb719a5e6a5777e99054ad5f5936d8dbc74f47
James Harrison <somtochukwujames2000@gmail.com>
2022-05-14 01:12:40 +0100

    chores : merkel proof commit

---
+++ Merkel.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.7;
+
+contract MerkelProof {
+    function verify (bytes32[] memory proof, bytes32 root, bytes32 leaf, uint index) public pure returns bool{
+        bytes32 hash = leaf;
+
+        for (uint i = 0; i < proof.length; i++) {
+            if (index % 2 == 0 ) {
+                hash = keccak256(abi.encodePacked(hash, proof[i])); // even => (current hash, proof element)
+            }
+            else {
+                 hash = keccak256(abi.encodePacked(proof[i], hash));  // odd => (proof element, current hash)
+            }
+
+            index = index / 2;   // updates the index and iterates again
+  
+        }
+        return hash = root;
+
+        
+    }
+}
